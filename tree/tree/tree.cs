99dc2be42d827817cf614f2837ed4977f33f4cb3
Unknown <jakob.most@hotmail.com>
2018-03-07 11:00:36 +0100

    This is the stack n queue thing

---
+++ tree/tree/tree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> root;
+
+        public Tree(T value)
+        {
+            if (value != null)
+            {
+                this.root = new TreeNode<T>(value);
+            }
+        }
+
+
+        public void BreadthFirstSearch()
+        {
+            //Create new queue
+            Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+            //Put root in queue
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                TreeNode<T> v = queue.Dequeue();
+                Console.WriteLine(v.value);
+                foreach (TreeNode<T> c in v.children)
+                {
+                    queue.Enqueue(c);
+                }
+            }
+        }
+
+        public void DepthFirstSearch()
+        {
+            Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+            stack.Push(root);
+            while (stack.Count != 0)
+            {
+                TreeNode<T> v = stack.Pop();
+                Console.WriteLine(v.value);
+                foreach (TreeNode<T> c in v.children)
+                {
+                    stack.Push(c);
+                }
+
+            }
+        }
+    }
+}
---
+++ tree/tree/treenode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree
+{
+    public class TreeNode<T>
+    {
+        public T value;
+        public bool hasParent;
+        public List<TreeNode<T>> children;
+
+        public TreeNode(T value)
+        {
+            if (value != null)
+            {
+                this.value = value;
+                this.children = new List<TreeNode<T>>();
+            }
+        }
+
+        public void AddChild(TreeNode<T> child)
+        {
+            child.hasParent = true;
+            this.children.Add(child);
+        }
+        public TreeNode<T> GetChild(int index)
+        {
+            return children[index];
+        }
+    }
+}
